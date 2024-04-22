7da4ea01dbea16ab42fa4e112ad1572587626872
emmawood <emmawood.coding@gmail.com>
2024-04-21 23:33:04 -0600

    .

--- codeExamples/week6/week6day3LECTUREgregslistVue/src/pages/CarsPage.vue
+++ codeExamples/week6/week6day3LECTUREgregslistVue/src/pages/CarsPage.vue
@@ -9,42 +9,42 @@ const cars = computed(() => AppState.cars)
 const account = computed(() => AppState.account)
 
 async function getCars() {
-  try {
-    await carsService.getCars()
-  }
-  catch (error) {
-    Pop.error(error);
-  }
+	try {
+		await carsService.getCars()
+	}
+	catch (error) {
+		Pop.error(error);
+	}
 }
 
 onMounted(() => {
-  getCars()
+	getCars()
 })
 
 </script>
 
 
 <template>
-  <div class="container">
-    <section class="row">
-      <div class="col-12 mt-3 d-flex align-items-center gap-4">
-        <h1>Cars</h1>
-        <!-- NOTE v-if="account != null" if the user is logged in, show the button -->
-        <button v-if="account" class="btn btn-outline-dark" title="Create a new car listing!" data-bs-toggle="modal"
-          data-bs-target="#carFormModal">
-          <i class="mdi mdi-plus"></i>
-        </button>
-      </div>
-    </section>
+	<div class="container">
+		<section class="row">
+			<div class="col-12 mt-3 d-flex align-items-center gap-4">
+				<h1>Cars</h1>
+				<!-- NOTE v-if="account != null" if the user is logged in, show the button -->
+				<button v-if="account" class="btn btn-outline-dark" title="Create a new car listing!" data-bs-toggle="modal"
+					data-bs-target="#carFormModal">
+					<i class="mdi mdi-plus"></i>
+				</button>
+			</div>
+		</section>
 
-    <section class="row">
-      <div v-for="car in cars" :key="car.id" class="col-12 mb-3">
-        <CarCard :car="car" />
-      </div>
-    </section>
-  </div>
+		<section class="row">
+			<div v-for="car in cars" :key="car.id" class="col-12 mb-3">
+				<CarCard :car="car" />
+			</div>
+		</section>
+	</div>
 
-  <CarFormModal />
+	<CarFormModal />
 </template>
 
 
