da8e4e44561a541a52d1056b268594307e296599
Emma Wood <emmawood.coding@gmail.com>
2024-05-20 09:34:47 -0600

    .

--- codeExamples/week10/week9C#IntroLECTURE/obj/Debug/net8.0/csharp_console_intro.AssemblyInfo.cs
+++ codeExamples/week10/week9C#IntroLECTURE/obj/Debug/net8.0/csharp_console_intro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_console_intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6bdcf8b09a6c74c72fa273a39fbb611b1cf0eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e58cdbf251faf9fd0dd6d5592ab14d5084e456e")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_console_intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_console_intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
