e4d335363c8b1e58a1a4212d4d4ee1a29d472e4e
Emma Wood <emmawood.coding@gmail.com>
2024-05-22 17:13:13 -0600

    changed color

--- codeExamples/week10/week9C#IntroLECTURE/spring24_csharp_console_intro-main/obj/Debug/net8.0/csharp_console_intro.AssemblyInfo.cs
+++ codeExamples/week10/week9C#IntroLECTURE/spring24_csharp_console_intro-main/obj/Debug/net8.0/csharp_console_intro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_console_intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d681d6d8cae718e9b2816d317a1176fa84bc5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4200686fd100dde99c9bd32751d113fa1e73eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_console_intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_console_intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
